7fb5ff8fc695d4779ffa66c057623a0dbe5e3106
Ary3n <3012212@edison.k12.nj.us>
2018-01-26 16:36:14 -0500

    Distance drive

--- src/org/usfirst/frc/team2554/robot/subsystems/Elevator.java
+++ src/org/usfirst/frc/team2554/robot/subsystems/Elevator.java
@@ -1,5 +1,9 @@
 package org.usfirst.frc.team2554.robot.subsystems;
 
+import org.usfirst.frc.team2554.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -10,6 +14,10 @@ public class Elevator extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
+	Victor eM1 = new Victor(RobotMap.elevator[0]);
+	Victor eM2 = new Victor(RobotMap.elevator[1]);
+	DigitalInput[] limit = new DigitalInput[]{ new DigitalInput(RobotMap.limitSwitches[0]), new DigitalInput(RobotMap.limitSwitches[1]),new DigitalInput(RobotMap.limitSwitches[2]),new DigitalInput(RobotMap.limitSwitches[3]),
+	new DigitalInput(RobotMap.limitSwitches[4]),new DigitalInput(RobotMap.limitSwitches[5])};
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
